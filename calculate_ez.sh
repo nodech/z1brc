515f79c35a1f81f540a895297a62da94c48c1d9f
Ezeki Zibzibadze <ez@jok.io>
2024-10-12 17:48:15 +0400

    refactor: move to one file

--- calculate_ez.sh
+++ calculate_ez.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 FILE=$1
-BATCH_SIZE=1000000
 
 if [ ! -f $FILE ]; then
     echo "File $FILE does not exist."
 fi
 
 # Run the script with the following command:
-deno run -A --unstable-worker-options ./ez/mod.ts $FILE $BATCH_SIZE
+deno run -A --unstable-worker-options ./ez/ez-v1.ts $FILE
